c44c91bde8e158f8d3bae2d0f301122e76b8f48e
aalshabaan <aalshabaan@gmail.com>
2019-12-22 12:43:26 +0100

    Added static background to the SUB engine

---
+++ source/graphics_sub.h
@@ -0,0 +1,18 @@
+/*
+ * graphics_sub.h
+ *
+ *  Created on: 22 Dec 2019
+ *      Author: Dev
+ */
+#include <nds.h>
+#ifndef GRAPHICS_SUB_H_
+#define GRAPHICS_SUB_H_
+
+/**
+ * Initializes the sub engine, activates the VRAM and the backgrounds, and draws the static background image.
+ */
+void initGraphicsSub(void);
+
+
+
+#endif /* GRAPHICS_SUB_H_ */
--- source/main.c
+++ source/main.c
@@ -1,13 +1,12 @@
 
+#include"graphics_sub.h"
 
 #include <nds.h>
 #include <stdio.h>
 
 int main(void) {
 	//Graphics
-    consoleDemoInit();
-    printf("\n7aidar, il fallait build\n");
-
+    initGraphicsSub();
     while(1)
         swiWaitForVBlank();	
 }
